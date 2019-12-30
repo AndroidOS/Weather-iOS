4d14c0f6a86c0064c1b3c6d0ab7885e4c2c0419e
AndroidOS <burrowsgit@gmail.com>
2019-12-31 06:25:48 +1100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Manuel Carvalho on 31/12/19.
+//  Copyright © 2019 Manuel Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
